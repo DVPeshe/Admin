a13a93a1f3ead13c80e8b9ae69508e7c19f3a552
dvpesh <dvpesh@mail.ru>
2023-03-25 01:03:55 +0300

    task 4 commit 2

--- Service1/src/main/java/ru/scrum/service1/controllers/Controller.java
+++ Service1/src/main/java/ru/scrum/service1/controllers/Controller.java
@@ -2,6 +2,7 @@ package ru.scrum.service1.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,10 @@ public class Controller {
     @GetMapping
     public ResponseEntity<?> getMessage() {
         return ResponseEntity.ok("Ok! Service1 is working.");
+    }
+
+    @GetMapping("/{a}")
+    public ResponseEntity<?> getMessage(@PathVariable int a) {
+        return ResponseEntity.ok("a = " + a);
     }
 }
